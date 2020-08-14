3152d274b283aeac8e359c7ba9f6a96228de37f6
EduardoVega04 <eduarvega04@hotmail.com>
2020-08-14 04:22:08 +0000

    Updates to city, state, dbstorage and basemodel

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 from os import getenv
 
 t_storage = getenv('HBNB_TYPE_STORAGE')
 
 """Conditional to check the storage type (file or database)"""
 if t_storage == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+from os import getenv
+from models.city import City
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class attributes"""
+    """State class attributes"""
     name = Column(String(128), nullable=False)
     __tablename__ = "states"
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
