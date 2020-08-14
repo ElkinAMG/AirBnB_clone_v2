4f6e7692d27dad95c08935f88c7ae8b18a86e755
EduardoVega04 <eduarvega04@hotmail.com>
2020-08-14 04:54:14 +0000

    Fix to User

--- models/user.py
+++ models/user.py
@@ -2,16 +2,15 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """Class attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
-    __tablename__ = "users"
+     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
