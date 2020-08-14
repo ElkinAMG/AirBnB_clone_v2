db0da1f84692e83d333ef006d2eebec4af766092
EduardoVega04 <eduarvega04@hotmail.com>
2020-08-14 04:37:09 +0000

    Fix to models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,14 +2,11 @@
 """This module instantiates an object of class FileStorage"""
 from os import getenv
 
-t_storage = getenv('HBNB_TYPE_STORAGE')
 
-"""Conditional to check the storage type (file or database)"""
-if t_storage == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
