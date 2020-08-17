1b127cac3c459217e17a9931e9450599715504b5
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-08-17 18:14:17 -0500

    MALPARIDA VIDA

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,27 +17,20 @@ def do_deploy(archive_path):
     """Make a deploy to a remote server.
     """
 
-    ret = False
-
-    if exists(archive_path):
-        try:
-            filename = archive_path.split('/')[-1]
-            un_path = "/data/web_static/releases/{}".format(
-                filename.split('.')[0]
-            )
-
-            put(archive_path, '/tmp')
-
-            run('mkdir -p {}'.format(un_path))
-
-            run('tar -xzf /tmp/{} -C {}'.format(filename, un_path))
-            run('rm /tmp/{}'.format(filename))
-            run('mv {0}/web_static/* {0}/'.format(un_path))
-            run('rm -rf /data/web_static/current')
-            run('ln -s {} /data/web_static/current'.format(un_path))
-
-            ret = True
-        except:
-            ret = False
-
-    return ret
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
