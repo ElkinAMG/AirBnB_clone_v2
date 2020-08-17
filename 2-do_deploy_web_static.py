0d53ab8f3904af54db03f0283c432a1c8992efca
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-08-17 17:34:54 -0500

    Final Update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,6 +32,7 @@ def do_deploy(archive_path):
 
             run('tar -xvzf /tmp/{} -C {}'.format(filename, un_path))
             run('rm /tmp/{}'.format(filename))
+            run('mv {0}/web_static/* {0}'.format(un_path))
             run('rm -rf /data/web_static/current')
             run('ln -s {} /data/web_static/current'.format(un_path))
 
