0fb8ad883e52c68431162ad926e2b7e3ef85808f
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-09-01 21:30:13 -0500

    Fixed Typo

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application.
 """
-from flask import flask, render_template
+from flask import Flask, render_template
 from models import *
 from models import storage
 
